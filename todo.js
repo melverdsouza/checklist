378258b6191eabfede5730c66cf189b0be916030
Melver Dsouza <melverken10@gmail.com>
2020-01-07 23:45:26 +0530

    checkbox works only delete button should work

--- todo.js
+++ todo.js
@@ -30,13 +30,15 @@ function newTask() {
         let del = document.createElement('button');
         complete.setAttribute('type', 'checkbox')
         complete.setAttribute('class', 'complete')
+        complete.setAttribute('onclick','checkedbox()')
+        // complete.setAttribute('onclick','uncheckedbox()')
         tag.setAttribute('class', 'ele')
         del.setAttribute('class', 'btn')
         tag.innerHTML = taskList[id-1];
         del.innerHTML = 'delete';
-        document.getElementsByClassName('task')[0].appendChild(complete)
-        document.getElementsByClassName('task')[0].appendChild(tag)
-        document.getElementsByClassName('task')[0].appendChild(del)        
+        document.getElementsByClassName('task')[id-2].appendChild(complete)
+        document.getElementsByClassName('task')[id-2].appendChild(tag)
+        document.getElementsByClassName('task')[id-2].appendChild(del)        
     }
 }
 
@@ -45,5 +47,20 @@ function submit() {
     taskWithId()
     newTask();
     resetForm();
+}
+
+function checkedbox() {
+    for(let x in taskList){
+        let complete = document.getElementsByClassName('complete')[Number(x)-1]  
+        if(complete.checked === true) {
+            // check which elements box is checked and pass in brackets
+            var check = document.getElementsByClassName("ele")[Number(x)-1];
+            check.style.setProperty("text-decoration", "line-through");    
+        }
+        else if(complete.checked === false) {
+            var check = document.getElementsByClassName("ele")[Number(x)-1];
+            check.style.setProperty("text-decoration", "none");    
+        }
+    }
 }
 
